6f0930082a595ca781ed50f47db466b6858f67c8
EC2 Default User <ec2-user@ip-172-31-21-107.us-west-2.compute.internal>
2021-02-24 01:49:07 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,90 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Layout, Menu } from 'antd';
+import GlobalStatistics from './GlobalStatistics.js';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const { SubMenu } = Menu;
+const { Header, Sider, Content } = Layout;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const wallets = [
+  "0x3933357c21a5858358895902605758b650e63c78",
+  "0x1ad5e3fec6c5bf6d3d3073273f0b987a428314aa",
+  "0x92B649957a1faDE32C05038070aC4517fd3e1990"
+]
+
+
+const SiderDemo = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+
+  const toggle = () => {  setCollapsed(!collapsed)  };
+  
+
+    return (
+      <Router>
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={collapsed}
+          style={{overflow:'auto', height:'100vh', left:0,}}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<UserOutlined />} to="/">
+              Global Statistics
+            </Menu.Item>
+            <SubMenu key="sub1" icon={<UserOutlined />} title="Wallets">
+              {wallets.map((wallet, index) => (<Menu.Item key={index+1}>{wallet}</Menu.Item>))}
+            </SubMenu>
+            <Menu.Item key="3" icon={<UploadOutlined />} to="/settings">
+              Settings
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: toggle,
+            })}
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+              overflow: 'initial'
+            }}
+          >
+            <Switch>
+              <Route path="/">
+                <GlobalStatistics/>
+              </Route>
+              <Route path="/workers">
+                Worker page
+              </Route>
+              <Route path="/settings">
+                Settings page
+              </Route>
+            </Switch>
+          </Content>
+        </Layout>
+      </Layout>
+      </Router>
+    );
+}
+
+ReactDOM.render(<SiderDemo />, document.getElementById('container'));
